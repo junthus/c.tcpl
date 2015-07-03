5f91f4fd3dd6e7d0a84df7c39057267de519c43a
juhee.bak <juhee.bak@NAVERui-iMac-54.local>
2015-07-03 18:54:28 +0900

    .

---
+++ ch02/console.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "console.h"
+
+int main()
+{
+	int inp;
+	
+	scanf("%d", &inp);
+	printf("%s\n", printb(inp));
+
+    return 0;
+}
---
+++ ch02/getbits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "console.h"
+
+unsigned getbits(unsigned x, int p, int n);
+
+int main()
+{
+    int inp, pos, num;
+
+    printf("input, position, num :");
+    scanf("%d %d %d", &inp, &pos, &num);
+
+    printf("%d, %d, %d, %s\n", inp, pos, num, printb(getbits(inp, pos, num)));
+	return 0;
+}
+
+
+unsigned getbits(unsigned x, int p, int n)
+{
+    return (x >> (p + 1 - n)) & ~(~0 << n);
+}
