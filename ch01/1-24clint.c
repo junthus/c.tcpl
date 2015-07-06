878e2d0f5cfff6bee8aea797787409c29dbdda44
juhee.bak <june@bakui-MacBook-Air.local>
2015-07-06 17:05:27 +0900

    .

---
+++ ch01/1-24clint.c
@@ -0,0 +1,14 @@
+//기본적인 c 문법을 검사하는 프로그램
+//짝이 맞지 않는 괄호, 중괄로, 인용부호, escape문자, 설명문 표시 등.
+
+#include <stdio.h>
+
+int main(){
+    int c;
+
+    while ((c = getchar()) != EOF) {
+
+    }
+
+    return 0;
+}
